01947695f488ecc7285dec6e91bbc7008734f355
nhu-hoan <162651637+nhu-hoan@users.noreply.github.com>
2024-03-08 08:30:01 +0700

    ok

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ngọc';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100025114350759';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Thanh Ngọc';
+const contentCard = 'Chúc m một 8-3 ngập tràn niềm vui và tiếng cười . Mong những điều tốt đẹp đến vơi m trong hôm nay và cả mai sau . Và t có điều muốn nói';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
